b8b3111dac148d4e8b88ac25cdd2c25085b6723a
Alexandra Donnelly <adonnelly246@gmail.com>
2021-10-07 11:26:18 -0500

    outline and basic setup

---
+++ client/frontend/src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact(){
+    return(
+        <div>
+            <h1>How to reach me</h1>
+           <p>email: adonnelly246@gmail.com</p> link?
+           <p></p>
+        </div>
+    )
+}
---
+++ client/frontend/src/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+ export default function Home (){
+     return(
+        <div className= "info">
+    <>
+        <h1>Who am I?</h1>
+        <h3>LinkedIn profile link</h3>
+        <h2>Alexandra Donnelly</h2>
+        <ul>
+            <li>Web Developer- </li>
+            <li>Leader-</li>
+            <li>Baker of Cookies-</li>
+        </ul>
+</>
+
+
+            <h1>What do I know?</h1>
+        <div>grid image display of technologies/skills- fontawesome 
+        <i class="fab fa-html5"></i> <i class="fab fa-react"></i> <i class="fab fa-node-js"></i> 
+        <i class="fab fa-js"></i> <i class="fab fa-github-square"></i>
+            <p>JavaScript, React, Redux, Git, Express, MongoDB, Node.js, HTML</p>
+        </div>
+    </div>
+     )
+ }
+ 
+// create an outlined box in a contrasing dark color around header/info. (green?)
